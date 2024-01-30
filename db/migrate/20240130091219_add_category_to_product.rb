a110151fcb5dbd73dc44419003e62e0544171629
abem93 <aimejia4@gmail.com>
2024-01-30 03:38:16 -0600

    Exercise 3 Completed

--- db/migrate/20240130091219_add_category_to_product.rb
+++ db/migrate/20240130091219_add_category_to_product.rb
@@ -3,7 +3,3 @@ class AddCategoryToProduct < ActiveRecord::Migration[7.1]
     add_reference :products, :category, foreign_key: true
   end
 end
-product = Product.find(1)
-category = Category.find(1)
-product.category = category
-product.save
